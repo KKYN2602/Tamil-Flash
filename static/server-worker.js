42100734842c249954711d91d252800cb1a58cfb
Karthikeyan S <118602686+KKYN2602@users.noreply.github.com>
2025-06-11 23:08:23 +0530

    Add files via upload

---
+++ static/server-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "tamilflash-cache-v1";
+
+const urlsToCache = [
+    "/",
+    "/alphabets",
+    "/numbers",
+    "/colors",
+    "/common_phrases",
+    "/static/css/style.css",
+    "/static/js/script.js",
+    "/static/icons/icon-192.png",
+    "/static/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
