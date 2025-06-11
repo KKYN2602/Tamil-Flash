65c632d041c4535cd589b5cc7cedb261bf2bd47c
Karthikeyan S <118602686+KKYN2602@users.noreply.github.com>
2025-06-11 23:15:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime  
+from datetime import datetime
+import json
 
 db = SQLAlchemy()
 
@@ -12,9 +12,23 @@ class User(db.Model):
     streak = db.Column(db.Integer, default=0)
     last_login = db.Column(db.DateTime, default=datetime.utcnow)
 
+    # New fields
+    progress = db.Column(db.Integer, default=0)
+    xp = db.Column(db.Integer, default=0)
+    badges = db.Column(db.Text, default='[]')  # Store JSON string list
+    activities = db.Column(db.Text, default='[]')  # Store JSON string list
+    join_date = db.Column(db.DateTime, default=datetime.utcnow)
+    profile_image_url = db.Column(db.String(300), nullable=True)
+
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
         self.streak = 0
         self.last_login = datetime.utcnow()
+        self.progress = 0
+        self.xp = 0
+        self.badges = json.dumps([])  # Store list as string
+        self.activities = json.dumps([])
+        self.join_date = datetime.utcnow()
+        self.profile_image_url = None
